5dd2e7e82e9d45225db5e1148b2f165d30636c64
Ilya Cherkasov <cherkasov-i@surfstudio.ru>
2021-12-15 18:11:19 +0300

    Initial Commit

---
+++ StreamQRCode/StreamQRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamQRCodeApp.swift
+//  StreamQRCode
+//
+//  Created by Ilya Cherkasov on 15.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct StreamQRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
